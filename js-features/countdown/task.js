26170ee66aea986c18c20d0cf6fcaf5fe9136e49
Sergey Karpaev <34karpaev@mail.ru>
2024-06-26 13:56:41 +0500

    first commit countdown

---
+++ js-features/countdown/task.js
@@ -0,0 +1,14 @@
+let timerId = document.getElementById('timer');
+function countDown() {
+  diff = timerId.textContent - 1;
+  if (diff >= 0) {
+    timerId.textContent = diff;
+    console.log(timerId.textContent);
+  }
+  else {
+    clearInterval(diff);
+    alert("Вы победили в конкурсе!");
+  }
+}
+diff = setInterval(countDown, 1000);
+countDown()
