88a1454e5767b3bf1c1ff52f70b0fbfa3fb784ce
Sergey Karpaev <34karpaev@mail.ru>
2024-06-28 14:27:59 +0500

    first commit cookie-clicker

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,7 @@
+const clickerr = document.getElementById("clicker__counter");
+const image = document.getElementById("cookie");
+function changeSizes() {
+image.width = 300;
+clickerr.textContent++;
+};
+cookie.onclick = changeSizes;
